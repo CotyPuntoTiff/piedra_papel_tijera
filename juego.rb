7f450cdcfb9122c29969ee6382a0aab275b18581
Constanza Mardones <coty.tiff@gmail.com>
2021-01-05 23:16:09 -0300

    arreglando condiciones

--- juego.rb
+++ juego.rb
@@ -1,40 +1,41 @@
 people = ARGV[0]
 pc = rand(3)
 
-if people == 'piedra' || people == 'papel' || people == 'tijera' 
-else puts 'Argumento invalido: Debe ser piedra, papel o tijera.'
-end
 
 pc = 'papel' if pc == 0
 pc = 'piedra' if pc == 1
 pc = 'tijera'  if pc == 2
 
-puts "computador juega #{pc}" 
 
-if     people == 'papel' 
-     if pc == 'piedra' 
-         puts 'Ganaste'
-     elsif pc == 'tijera'
-         puts 'Perdiste'
-     else pc == 'papel'
-         puts 'Empataste'
-     end
-    
-elsif     people == 'piedra' 
-     if pc == 'tijera' 
-         puts 'Ganaste'
-     elsif pc == 'papel'
-         puts 'Perdiste'
-     else pc == 'piedra'
-         puts 'Empataste'
-     end
-
-else     people == 'tijera' 
-     if pc == 'papel' 
-         puts 'Ganaste'
-     elsif pc == 'piedra'
-         puts 'Perdiste'
-     else pc == 'tijera'
-         puts 'Empataste'
-     end
+if people == 'piedra' || people == 'papel' || people == 'tijera' 
+    puts "computador juega #{pc}" 
+    if     people == 'papel' 
+        if pc == 'piedra' 
+            puts 'Ganaste'
+        elsif pc == 'tijera'
+            puts 'Perdiste'
+        else pc == 'papel'
+            puts 'Empataste'
+        end
+        
+    elsif     people == 'piedra' 
+        if pc == 'tijera' 
+            puts 'Ganaste'
+        elsif pc == 'papel'
+            puts 'Perdiste'
+        else pc == 'piedra'
+            puts 'Empataste'
+        end
+        
+    else     people == 'tijera' 
+        if pc == 'papel' 
+            puts 'Ganaste'
+        elsif pc == 'piedra'
+            puts 'Perdiste'
+        else pc == 'tijera'
+            puts 'Empataste'
+        end
+    end
+else puts 'Argumento invalido: Debe ser piedra, papel o tijera.'
 end
+    
