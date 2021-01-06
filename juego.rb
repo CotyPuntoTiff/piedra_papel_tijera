b5dae34fa1be3ff7d39323d41e1da5c5eeafd637
Constanza Mardones <coty.tiff@gmail.com>
2021-01-05 21:24:01 -0300

    agrege el argumento invalido

---
+++ juego.rb
@@ -0,0 +1,11 @@
+people = ARGV[0]
+pc = rand (3)
+
+if people == 'piedra' || people == 'pepel' || people == 'tijera' 
+else puts 'Argumento invalido: Debe ser piedra, papel o tijera.'
+end
+
+papel = 
+piedra
+tijera
+
