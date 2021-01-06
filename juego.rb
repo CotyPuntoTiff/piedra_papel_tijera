43a2bd58a9d299591c3f0db20e183872e0626fef
Constanza Mardones <coty.tiff@gmail.com>
2021-01-05 23:10:54 -0300

    terminado

--- juego.rb
+++ juego.rb
@@ -1,11 +1,40 @@
 people = ARGV[0]
-pc = rand (3)
+pc = rand(3)
 
-if people == 'piedra' || people == 'pepel' || people == 'tijera' 
+if people == 'piedra' || people == 'papel' || people == 'tijera' 
 else puts 'Argumento invalido: Debe ser piedra, papel o tijera.'
 end
 
-papel = 
-piedra
-tijera
+pc = 'papel' if pc == 0
+pc = 'piedra' if pc == 1
+pc = 'tijera'  if pc == 2
 
+puts "computador juega #{pc}" 
+
+if     people == 'papel' 
+     if pc == 'piedra' 
+         puts 'Ganaste'
+     elsif pc == 'tijera'
+         puts 'Perdiste'
+     else pc == 'papel'
+         puts 'Empataste'
+     end
+    
+elsif     people == 'piedra' 
+     if pc == 'tijera' 
+         puts 'Ganaste'
+     elsif pc == 'papel'
+         puts 'Perdiste'
+     else pc == 'piedra'
+         puts 'Empataste'
+     end
+
+else     people == 'tijera' 
+     if pc == 'papel' 
+         puts 'Ganaste'
+     elsif pc == 'piedra'
+         puts 'Perdiste'
+     else pc == 'tijera'
+         puts 'Empataste'
+     end
+end
